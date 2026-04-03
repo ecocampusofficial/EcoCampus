29b0875191eacfc4d13860f93797dc3bafde5e67
EcoCamous <ecocampusoffical@gmail.com>
2026-04-03 15:32:40 +0530

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -3,9 +3,8 @@
 const SUPABASE_URL = "https://rreczghlcgsrcmfjpzdo.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_TsYRWSpXaEnvopMDdzd36Q_N4TbMymz";
 
-// create client
-const supabase = window.supabase.createClient(
+// store in global
+window.sb = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
-
